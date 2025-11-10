cc7c8ecad0fbba0ffd071083a27a70fb815bcd5c
Matthew Lundquist <lundquistecologylab@gmail.com>
2025-11-10 12:41:17 -0500

    nix 25.05

--- flake.nix
+++ flake.nix
@@ -2,10 +2,10 @@
   description = "NixOS configuration to mirror the existing Arch setup on moria";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
     home-manager = {
-      url = "github:nix-community/home-manager/release-24.05";
+      url = "github:nix-community/home-manager/release-25.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     sysc-greet-src = {
